fd8294e75d54a54b5c8704d873cf3317e7304957
Andrii Bodnar <andrii.bodnar@crowdin.com>
2021-06-01 15:13:27 +0300

    Version 1.2.0

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export class Constants {
     static readonly OPEN_TMS_FILE_COMMAND = 'extension.openTmsFile';
     static readonly AUTO_REFRESH_PROPERTY = 'tms.autoRefresh';
     static readonly CROWDIN_PATH_SEPARATOR = '/';
-    static readonly PLUGIN_VERSION = '1.1.1';
+    static readonly PLUGIN_VERSION = '1.2.0';
     static readonly CLIENT_RETRIES = 5;
     static readonly CLIENT_RETRY_WAIT_INTERVAL_MS = 750;
     static VSCODE_VERSION: string;
