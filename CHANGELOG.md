b8665739fb63b2d491c61f1941eafea465935e5d
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-10-30 11:58:56 +0200

    chore: version 2.2.3 (#203)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to the Crowdin Visual Studio Code Plugin extension will be d
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.2.3]
+
+- Fix bundles url
+- Dependencies update
+
 ## [2.2.2]
 
 - Fix file select
--- src/constants.ts
+++ src/constants.ts
@@ -37,7 +37,7 @@ export class Constants {
     static readonly USE_GIT_BRANCH_PROPERTY = 'crowdin.useGitBranch';
     //general
     static readonly CROWDIN_PATH_SEPARATOR = '/';
-    static readonly PLUGIN_VERSION = '2.2.2';
+    static readonly PLUGIN_VERSION = '2.2.3';
     static readonly CLIENT_RETRIES = 5;
     static readonly CLIENT_RETRY_WAIT_INTERVAL_MS = 750;
     static VSCODE_VERSION: string;
